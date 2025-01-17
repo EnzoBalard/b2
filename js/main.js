ebd84881ab81e53e135bc623d7bca9e311c10013
Enzo Balard <enzobalard@MacBook-Pro-de-Enzo.local>
2025-01-17 13:34:09 +0100

    Adding GSAP files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// GSAP Modules
+gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
+
+// gsap demo   animation
+gsap.to("#Demo",{
+    rotation: 360,
+    duration: 2,
+})
